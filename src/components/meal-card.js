873e8299509c34d259dce94bf146d7da2b53bff9
Ryan Fritsch <ryanjfritsch@gmail.com>
2019-09-04 20:55:38 -0700

    Convert hi-res carousel images to 50% size, and header images to 50% JPG.

--- src/components/meal-card.js
+++ src/components/meal-card.js
@@ -1,50 +1,51 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 // import Img from 'gatsby-image';
 
 import StarsRating from '../components/starsRating.js'
 
-import './starsRating.css';
+import './starsRating.css'
 
 import './meal-card.css'
 
-
 export default class MealCard extends Component {
-
-    constructor(props) {
-        super();
-        this.state = { 
-            rating: props.rating,
-            imageName: props.imageName,
-            date: props.date
-        };
-
-      }
-
-      UNSAFE_componentWillReceiveProps(props) {
-        this.setState({ 
-            rating: props.rating,
-            imageName: props.imageName,
-            date: props.date
-        });
-      }
-
-    render() {
-
-        const mealImage = require('../images/headers/'+this.props.imageName+'.png');
-
-        return(
-
-            <div className="mealCard">
-                <div>
-                    <img className="cardImage" src={mealImage} alt="" style={{ width: "300", height: "300" }} />
-                </div>
-                <div className="mealCardLowerHalf">
-                    <span className="mealCardTitle">{ this.props.title }</span>
-                    <span className="mealCardDate">{ this.props.date }</span>
-                    <StarsRating rating={ this.state.rating }></StarsRating>
-                </div>
-            </div>
-        );
-
+  constructor(props) {
+    super()
+    this.state = {
+      rating: props.rating,
+      imageName: props.imageName,
+      date: props.date,
     }
+  }
+
+  UNSAFE_componentWillReceiveProps(props) {
+    this.setState({
+      rating: props.rating,
+      imageName: props.imageName,
+      date: props.date,
+    })
+  }
+
+  render() {
+    const mealImage = require('../images/headers/' +
+      this.props.imageName +
+      '.jpg')
+
+    return (
+      <div className="mealCard">
+        <div>
+          <img
+            className="cardImage"
+            src={mealImage}
+            alt=""
+            style={{ width: '300', height: '300' }}
+          />
+        </div>
+        <div className="mealCardLowerHalf">
+          <span className="mealCardTitle">{this.props.title}</span>
+          <span className="mealCardDate">{this.props.date}</span>
+          <StarsRating rating={this.state.rating} />
+        </div>
+      </div>
+    )
+  }
 }
