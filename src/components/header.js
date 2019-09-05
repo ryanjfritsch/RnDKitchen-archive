274db92f0efe8148f00db938d6eee8718bf4ea26
Ryan Fritsch <ryanjfritsch@gmail.com>
2019-09-04 21:07:46 -0700

    Change all-meals route to all-recipes

--- src/components/header.js
+++ src/components/header.js
@@ -2,32 +2,39 @@ import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
 import React from 'react'
 
-import './header.css';
-import circleLogo from "../images/circleLogoSM.png";
+import './header.css'
+import circleLogo from '../images/circleLogoSM.png'
 // import circleTextLogo from "../images/circleTextLogoSM.png";
-
 
 const Header = ({ siteTitle }) => (
   <div className="header">
     <div className="headerTextItems">
-      <Link to="/" 
+      <Link
+        to="/"
         style={{ textDecoration: 'none' }}
-        activeStyle={{ cursor: 'default' }}>
+        activeStyle={{ cursor: 'default' }}
+      >
         {/* <span id="FYRlogo">R+D</span> */}
         <img src={circleLogo} id="fyrLogo" alt="" />
         <span id="fyrText">Kitchen</span>
       </Link>
       <div className="subHeaderText" id="rightHeaderText">
-          <Link to="all-meals"
-            className="headerNav"
-            activeClassName="headerNav"
-            activeStyle={{ color: '#E8E8EB', cursor: 'default' }}
-            >All Recipes</Link>
-          <Link to="/#about"
-            className="headerNav"
-            activeClassName="headerNav"
-            activeStyle={{ color: '#E8E8EB', cursor: 'default' }}
-            >About</Link>
+        <Link
+          to="all-recipes"
+          className="headerNav"
+          activeClassName="headerNav"
+          activeStyle={{ color: '#E8E8EB', cursor: 'default' }}
+        >
+          All Recipes
+        </Link>
+        <Link
+          to="/#about"
+          className="headerNav"
+          activeClassName="headerNav"
+          activeStyle={{ color: '#E8E8EB', cursor: 'default' }}
+        >
+          About
+        </Link>
       </div>
     </div>
   </div>
